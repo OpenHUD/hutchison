d393e011829d832f59c88c8b97a14531d8210e79
Danny Leshem <dleshem@gmail.com>
2019-01-07 14:48:36 +0200

    Fixed typo in test

--- test/omahaHoldem.test.js
+++ test/omahaHoldem.test.js
@@ -1,29 +1,29 @@
-import texasHoldem from '../src/omahaHoldem.js';
+import omahaHoldem from '../src/omahaHoldem.js';
 import {expect} from 'chai';
 
 describe('omahaHoldem', () => {
     it ('correctly calculates AAKK double suited', () => {
         // Example in Huchinson's original article
-        expect(texasHoldem({hand: ['As', 'Ks', 'Ah', 'Kh']})).to.equal(54);
+        expect(omahaHoldem({hand: ['As', 'Ks', 'Ah', 'Kh']})).to.equal(54);
     });
 
     it ('correctly calculates 9988 double suited', () => {
         // Example in Huchinson's original article
-        expect(texasHoldem({hand: ['9s', '8s', '9d', '8d']})).to.equal(32);
+        expect(omahaHoldem({hand: ['9s', '8s', '9d', '8d']})).to.equal(32);
     });
 
     it ('correctly calculates QQ99 rainbow', () => {
         // Example in Huchinson's original article
-        expect(texasHoldem({hand: ['Qs', 'Qd', '9h', '9c']})).to.equal(28);
+        expect(omahaHoldem({hand: ['Qs', 'Qd', '9h', '9c']})).to.equal(28);
     });
 
     it ('correctly calculates AKQT suited', () => {
         // Example in Huchinson's original article calculates this as 31, but forgets to subtract 4 for the straighty ace
-        expect(texasHoldem({hand: ['As', 'Kd', 'Qh', 'Ts']})).to.equal(27);
+        expect(omahaHoldem({hand: ['As', 'Kd', 'Qh', 'Ts']})).to.equal(27);
     });
 
     it ('correctly calculates AK43 double suited', () => {
         // Example in Huchinson's original article
-        expect(texasHoldem({hand: ['As', '3s', 'Kd', '4d']})).to.equal(30);
+        expect(omahaHoldem({hand: ['As', '3s', 'Kd', '4d']})).to.equal(30);
     });
 });
