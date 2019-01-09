173e926540163f6ebcb0be7fb5404d8ca49b5cbb
Danny Leshem <dleshem@gmail.com>
2019-01-10 01:47:12 +0200

    Updated PLO scoring algorithm based on new reference implementation

--- src/omahaHoldem.js
+++ src/omahaHoldem.js
@@ -28,59 +28,55 @@ sortedHistogram.forEach((count, points) => {
 
 JSON.stringify([...cdf.entries()])
 */
-const cdf = new Map([[
-    -2,0.000029550281651121987],
-    [0,0.0010527287838212208],
-    [1,0.0011413796287745867],
-    [2,0.0017028349801459046],
-    [3,0.002500692584726198],
-    [4,0.0034167513159109797],
-    [5,0.004569212300304737],
-    [6,0.006504755748453227],
-    [7,0.008455074337427279],
-    [8,0.012326161233724259],
-    [9,0.017246283128636068],
-    [10,0.026746698679471787],
-    [11,0.03837473450918829],
-    [12,0.057848370117277684],
-    [13,0.08267060670422015],
-    [14,0.11942376950780312],
-    [15,0.16270015698587126],
-    [16,0.22437898236217563],
-    [17,0.28859174439006374],
-    [18,0.37466432726936927],
-    [19,0.4563265306122449],
-    [20,0.5491144149967679],
-    [21,0.6232117462369563],
-    [22,0.7051103518330409],
-    [23,0.7620611321451658],
-    [24,0.8187902853449072],
-    [25,0.855617323852618],
-    [26,0.8909225228552959],
-    [27,0.9139348046911072],
-    [28,0.9340068335026318],
-    [29,0.9469867947178872],
-    [30,0.9607276756856589],
-    [31,0.9670883738110629],
-    [32,0.9747123464770524],
-    [33,0.9787902853449072],
-    [34,0.985394773293933],
-    [35,0.9877440206851972],
-    [36,0.9926641425801089],
-    [37,0.993572813740881],
-    [38,0.9958998984209069],
-    [39,0.9963431526456736],
-    [40,0.9977394034536892],
-    [41,0.9979388678548342],
-    [42,0.9988697017268446],
-    [43,0.9990026779942747],
-    [44,0.9996010711977098],
-    [45,0.9996232339089482],
-    [46,0.9998448610213316],
-    [47,0.9998891864438083],
-    [48,0.999933511866285],
-    [50,0.9999778372887617],
-    [54,1]
+const cdf = new Map([
+    [0,0.0013334564595068796],
+    [0.5,0.0013925570228091237],
+    [1,0.006342229199372057],
+    [1.5,0.007258287930556838],
+    [2,0.028327638747806815],
+    [2.5,0.030617785575768768],
+    [3,0.054642164558130944],
+    [3.5,0.06244343891402715],
+    [4,0.08580293655923908],
+    [4.5,0.09486009788530797],
+    [5,0.13323113860928987],
+    [5.5,0.14686859359128265],
+    [6,0.21510019392372334],
+    [6.5,0.2415920214239542],
+    [7,0.31611783174808383],
+    [7.5,0.3629550281651122],
+    [8,0.43934989380367534],
+    [8.5,0.4872804506417952],
+    [9,0.5563025210084034],
+    [9.5,0.5919253855388309],
+    [10,0.658694246929541],
+    [10.5,0.6836642349247392],
+    [11,0.7396915689352664],
+    [11.5,0.7625043863699326],
+    [12,0.8121710222550559],
+    [12.5,0.8363431526456736],
+    [13,0.8681614184135192],
+    [13.5,0.8879748822605965],
+    [14,0.9216104903499861],
+    [14.5,0.9301209714655093],
+    [15,0.9521728691476591],
+    [15.5,0.9575805706898144],
+    [16,0.9676646043032597],
+    [16.5,0.9719198448610213],
+    [17,0.9778372887616585],
+    [17.5,0.9794330039708191],
+    [18,0.9875445562840521],
+    [18.5,0.9882094376212023],
+    [19,0.9916668205743836],
+    [19.5,0.9919770985317203],
+    [20,0.9971188475390156],
+    [20.5,0.9972518238064456],
+    [21,0.998116169544741],
+    [22,0.9986924000369378],
+    [23,0.9990691661279897],
+    [24,0.999933511866285],
+    [25,0.9999778372887617],
+    [27,1]
 ]);
 
 const rankIndices = new Map([
@@ -100,13 +96,13 @@ const rankIndices = new Map([
 ]);
 
 const suitedRankPoints = new Map([
-    ['A', 8],
-    ['K', 6],
-    ['Q', 5],
-    ['J', 4],
-    ['T', 3],
-    ['9', 3],
-    ['8', 2],
+    ['A', 4],
+    ['K', 3],
+    ['Q', 2.5],
+    ['J', 2],
+    ['T', 1.5],
+    ['9', 1.5],
+    ['8', 1],
     ['7', 1],
     ['6', 1],
     ['5', 1],
@@ -116,22 +112,148 @@ const suitedRankPoints = new Map([
 ]);
 
 const pairedRankPoints = new Map([
-    ['A', 18],
-    ['K', 16],
-    ['Q', 14],
-    ['J', 13],
-    ['T', 12],
-    ['9', 10],
-    ['8', 8],
-    ['7', 7],
-    ['6', 7],
-    ['5', 7],
-    ['4', 7],
-    ['3', 7],
-    ['2', 7]
+    ['A', 9],
+    ['K', 8],
+    ['Q', 7],
+    ['J', 6],
+    ['T', 6],
+    ['9', 5],
+    ['8', 4],
+    ['7', 4],
+    ['6', 4],
+    ['5', 4],
+    ['4', 4],
+    ['3', 4],
+    ['2', 4]
 ]);
 
-// Based on http://erh.homestead.com/omaha.html
+const gaploss = new Map([
+    [0, 0],
+    [1, 1],
+    [2, 1],
+    [3, 2],
+    [4, 0],
+    [5, 0],
+    [6, 0],
+    [7, 0],
+    [8, 0],
+    [9, 0],
+    [10, 0],
+    [11, 0],
+    [12, 0],
+    [13, 0]
+]);
+const calcGap = rankIndices => rankIndices[0] - rankIndices[rankIndices.length - 1] + 1 - rankIndices.length;
+
+const ace = rankIndices => rankIndices.filter(rankIndex => rankIndex === 14);
+const court = rankIndices => rankIndices.filter(rankIndex => rankIndex > 9 && rankIndex < 14);
+const twoSix = rankIndices => rankIndices.filter(rankIndex => rankIndex > 1 && rankIndex < 7);
+const twoFive = rankIndices => rankIndices.filter(rankIndex => rankIndex > 1 && rankIndex < 6);
+const sixUp = rankIndices => rankIndices.filter(rankIndex => rankIndex > 5);
+const sixKing = rankIndices => rankIndices.filter(rankIndex => rankIndex > 5 && rankIndex < 14);
+
+const scoreFourHighCards = (rankIndices) => {
+    const highs = sixUp(rankIndices);
+    if (highs.length === 4) {
+        const gap = calcGap(highs);
+        if (gap > 3) {
+            return 0;
+        } else {
+            return 12 - gaploss.get(gap);
+        }
+    } else {
+        return 0;
+    }
+};
+
+const scoreAceLow = (rankIndices) => (ace(rankIndices).length > 0 && twoFive(rankIndices).length > 0) ? 1 : 0;
+
+const scoreTwoLowCards = (rankIndices) => {
+    const lows = twoSix(rankIndices);
+    if (lows.length >= 2) {
+        const gap = calcGap(lows);
+        if (gap > 3) {
+            return 0;
+        } else {
+            return 2 - gaploss.get(gap);
+        }
+    } else {
+        return 0;
+    }
+};
+
+const scoreThreeHighCards = (rankIndices) => {
+    const highs = sixUp(rankIndices);
+    if (highs.length >= 3) {
+        if (highs.length === 3) {
+            const gap = calcGap(highs);
+            return 7 - gaploss.get(gap);
+        } else {
+            const hi = [highs[0], highs[1], highs[2]];
+            const lo = [highs[1], highs[2], highs[3]];
+            return Math.max(calcStraightPoints(hi), calcStraightPoints(lo));
+        }
+    } else {
+        return 0;
+    }
+};
+
+const scoreTwoHighCards = (rankIndices) => {
+    const highs = sixKing(rankIndices);
+    if (highs.length === 2) {
+        const gap = calcGap(highs);
+        if (gap > 3) {
+            return 0;
+        } else {
+            return 4 - gaploss.get(gap);
+        }
+    } else {
+        return 0;
+    }
+};
+
+const scoreAceCourt = (rankIndices) => {
+    const aces = ace(rankIndices);
+    const courts = court(rankIndices);
+    if (aces.length > 0 && courts.length > 0) {
+        const gap = calcGap(aces.concat(courts));
+        if (gap > 3) {
+            return 0;
+        } else {
+            return 2 - gaploss.get(gap);
+        }
+    } else {
+        return 0;
+    }
+};
+
+const calcStraightPoints = (rankIndices) => {
+    let points = 0;
+
+    points += scoreFourHighCards(rankIndices);
+    if (points === 0) {
+        points += scoreAceLow(rankIndices);
+        points += scoreTwoLowCards(rankIndices);
+
+        const high3 = scoreThreeHighCards(rankIndices);
+        points += high3;
+        if (high3 === 0) {
+            const high2 = scoreTwoHighCards(rankIndices);
+            points += high2;
+            if (high2 === 0) {
+                points += scoreAceCourt(rankIndices);
+            }
+        }
+    }
+
+    return points;
+};
+
+
+// Based on https://www.scribd.com/document/49697747/Edward-Hutchison-Hutchinson-Omaha-Point-System
+// Straight scoring is ported from https://metacpan.org/source/TMTM/Games-Poker-Omaha-Hutchison-1.04/lib/Games/Poker/Omaha/Hutchison.pm#PGames::Poker::Omaha::Hutchison::StraightScorer
+//
+// For a different version, see http://erh.homestead.com/omaha.html
 const omahaHoldem = ({hand}) => {
     if (!hand || hand.length !== 4) {
         throw new Error('Invalid omaha holdem hand');
@@ -155,8 +277,8 @@ const omahaHoldem = ({hand}) => {
     ranksBySuit.forEach(ranks => {
         if (ranks.length >= 2) {
             points += Math.max(...ranks.map(rank => suitedRankPoints.get(rank)));
-            if (ranks.length >= 3) {
-                points -= 2 ;
+            if (ranks.length === 4) {
+                points -= 2;
             }
         }
     });
@@ -194,54 +316,14 @@ const omahaHoldem = ({hand}) => {
     hand.forEach(([rank]) => {
         uniqueRanks.add(rank);
     });
+
     const sortedRankIndices = [...uniqueRanks].map(rank => rankIndices.get(rank));
     sortedRankIndices.sort((a, b) => b - a);
 
-    let straightPairs = 0;
-    let gaps = 0;
-    let straightAce = false;
-    for (let i = 0; i < sortedRankIndices.length - 1; ++i) {
-        const rankIndex1 = sortedRankIndices[i];
-        const rankIndex2 = sortedRankIndices[i+1];
+    points += calcStraightPoints(sortedRankIndices);
 
-        const gap = rankIndex1 - rankIndex2 - 1;
-        if (gap <= 3) {
-            ++straightPairs;
-            gaps += gap;
-            if (rankIndex1 === 14) {
-                straightAce = true;
-            }
-        }
-    }
-    switch(straightPairs) {
-    case 3:
-        points += 25;
-        break;
-    case 2:
-        points += 18;
-        break;
-    case 1:
-        points += 8;
-        break;
-    default:
-        break;
-    }
-    points -= 2 * gaps;
-    if (straightAce) {
-        points -= 4;
-    }
-
-    if (uniqueRanks.has('A')) {
-        const wheelCards =
-            uniqueRanks.has('2') ? 1 : 0 +
-            uniqueRanks.has('3') ? 1 : 0 +
-            uniqueRanks.has('4') ? 1 : 0 +
-            uniqueRanks.has('5') ? 1 : 0;
-        points += Math.min(12, 2 * wheelCards);
-    }
-    
+    // Return
     const percentile = cdf.get(points);
-
     return {points, percentile};
   };
 
--- test/omahaHoldem.test.js
+++ test/omahaHoldem.test.js
@@ -1,39 +1,100 @@
 import omahaHoldem from '../src/omahaHoldem.js';
 import {expect} from 'chai';
 
+// Tests based on examples from:
+// [1] http://erh.homestead.com/omaha.html
+// [2] https://www.scribd.com/document/49697747/Edward-Hutchison-Hutchinson-Omaha-Point-System
+// [3] https://metacpan.org/source/TMTM/Games-Poker-Omaha-Hutchison-1.04/t/01.t
 describe('omahaHoldem', () => {
     it ('correctly calculates AAKK double suited', () => {
-        // Example in Huchinson's original article
+        // From [1]
         const {points, percentile} = omahaHoldem({hand: ['As', 'Ks', 'Ah', 'Kh']});
-        expect(points).to.equal(54);
+        expect(points).to.equal(27);
         expect(percentile).to.equal(1);
     });
 
     it ('correctly calculates 9988 double suited', () => {
-        // Example in Huchinson's original article
+        // From [1]
         const {points, percentile} = omahaHoldem({hand: ['9s', '8s', '9d', '8d']});
-        expect(points).to.equal(32);
-        expect(percentile).to.equal(0.9747123464770524);
+        expect(points).to.equal(16);
+        expect(percentile).to.equal(0.9676646043032597);
     });
 
-    it ('correctly calculates QQ99 rainbow', () => {
-        // Example in Huchinson's original article
-        const {points, percentile} = omahaHoldem({hand: ['Qs', 'Qd', '9h', '9c']});
-        expect(points).to.equal(28);
-        expect(percentile).to.equal(0.9340068335026318);
+    it ('correctly calculates QQ88 rainbow', () => {
+        // From [2]
+        const {points, percentile} = omahaHoldem({hand: ['Qs', 'Qd', '8h', '8c']});
+        expect(points).to.equal(13);
+        expect(percentile).to.equal(0.8681614184135192);
+    });
+
+    it ('correctly calculates AA72 rainbow', () => {
+        // From [3]
+        const {points, percentile} = omahaHoldem({hand: ['As', 'Ah', '7c', '2d']});
+        expect(points).to.equal(10);
+        expect(percentile).to.equal(0.658694246929541);
+    });
+
+    it ('correctly calculates KK63 double suited', () => {
+        // From [3]
+        const {points, percentile} = omahaHoldem({hand: ['Ks', 'Kd', '3s', '6d']});
+        expect(points).to.equal(15);
+        expect(percentile).to.equal(0.9521728691476591);
     });
 
     it ('correctly calculates AKQT suited', () => {
-        // Example in Huchinson's original article calculates this as 31, but forgets to subtract 4 for the straighty ace
+        // From [2]
         const {points, percentile} = omahaHoldem({hand: ['As', 'Kd', 'Qh', 'Ts']});
-        expect(points).to.equal(27);
-        expect(percentile).to.equal(0.9139348046911072);
+        expect(points).to.equal(15);
+        expect(percentile).to.equal(0.9521728691476591);
     });
 
-    it ('correctly calculates AK43 double suited', () => {
-        // Example in Huchinson's original article
-        const {points, percentile} = omahaHoldem({hand: ['As', '3s', 'Kd', '4d']});
-        expect(points).to.equal(30);
-        expect(percentile).to.equal(0.9607276756856589);
+    it ('correctly calculates AAKQ double suited', () => {
+        // From [3]
+        const {points, percentile} = omahaHoldem({hand: ['Ah', 'Kh', 'Ad', 'Qd']});
+        expect(points).to.equal(24);
+        expect(percentile).to.equal(0.999933511866285);
+    });
+
+    it ('correctly calculates 5432 rainbow', () => {
+        // From [3]
+        const {points, percentile} = omahaHoldem({hand: ['2h', '3c', '4s', '5d']});
+        expect(points).to.equal(2);
+        expect(percentile).to.equal(0.028327638747806815);
+    });
+
+    it ('correctly calculates KQ32 rainbow', () => {
+        // From [3]
+        const {points, percentile} = omahaHoldem({hand: ['2h', '3c', 'Ks', 'Qd']});
+        expect(points).to.equal(6);
+        expect(percentile).to.equal(0.21510019392372334);
+    });
+
+    it ('correctly calculates A432 rainbow', () => {
+        // From [3]
+        const {points, percentile} = omahaHoldem({hand: ['Ah', '2c', '3s', '4d']});
+        expect(points).to.equal(3);
+        expect(percentile).to.equal(0.054642164558130944);
+    });
+
+
+    it ('correctly calculates AKQ2 rainbow', () => {
+        // From [3]
+        const {points, percentile} = omahaHoldem({hand: ['Ad', 'Kc', 'Qh', '2s']});
+        expect(points).to.equal(8);
+        expect(percentile).to.equal(0.43934989380367534);
+    });
+
+    it ('correctly calculates KJT6 rainbow', () => {
+        // From [3]
+        const {points, percentile} = omahaHoldem({hand: ['Kh', 'Jc', 'Th', '6s']});
+        expect(points).to.equal(9);
+        expect(percentile).to.equal(0.5563025210084034);
+    });
+
+    it ('correctly calculates AA64 worse suit', () => {
+        // From [3]
+        const {points, percentile} = omahaHoldem({hand: ['Ad', '6c', 'As', '4c']});
+        expect(points).to.equal(12);
+        expect(percentile).to.equal(0.8121710222550559);
     });
 });
