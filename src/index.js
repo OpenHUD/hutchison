5c762aada4cdb541c2f4b891ba4e8b82fce441b2
Danny Leshem <dleshem@gmail.com>
2019-08-02 14:57:10 +0300

    Added simulation-based omahaHoldem6

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import texasHoldem from './texasHoldem.js';
 import omahaHoldem from './omahaHoldem.js';
+import omahaHoldem6 from './omahaHoldem6.js';
 
-export {texasHoldem, omahaHoldem};
+export {texasHoldem, omahaHoldem, omahaHoldem6};
